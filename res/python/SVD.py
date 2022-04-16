c51ae66254b2d41a24f387cfcbeb3ab5eafe96e6
nckpro <69128990+nckpro@users.noreply.github.com>
2022-04-16 01:59:09 -0400

    Update SVD.py
    Implemented sorting the singular values and associated vectors. Also gave an example of doing the approximation up to rank k

--- res/python/SVD.py
+++ res/python/SVD.py
@@ -6,8 +6,25 @@ Created on Mon Apr 11 00:15:14 2022
 """
 import numpy as np
 from numpy.linalg import eig
+from numpy.linalg import norm
 
-#do we want to implement a heap or BST to keep the things sorted?
+
+"""
+Possible optimizations - filter out all the things that are possibly 0's to be just
+0's and remove them outright (problems include modifying 'n' variable and being able
+to distinguish between actual eigenvalues in all cases
+"""
+
+class triple:
+    def __init__(self, _sigma, _v, _u):
+        #sigma is a scalar
+        self.sigma = _sigma
+        #v is a nx1 vector
+        self.v = _v
+        #u is a mx1 vector
+        self.u = _u
+    #This is all we need for a triple, will want to make a list of n triples and
+    #sort by sigma
 
 #this will be a class that takes in a matrix and stores it as an "SVD" object
 #constructor will take in a matrix A of dimensions m x n and construct the 
@@ -28,28 +45,45 @@ class SVD:
         """
         self.sigma = np.sqrt(np.abs(self.sigma)) 
         self.u = np.zeros((self.m,self.n))
+        self.tripleList = []
         #This should compute n columns of A*v_i/sigma_i
         for i in range(self.n):
             self.u[:,i] = np.dot(A,self.v[:,i]) / self.sigma[i] 
+            self.tripleList.append(triple(self.sigma[i], self.v[:,i], self.u[:,i]))
         #now we have all of our singular values
         #depending on how we want to store them, we can do a variety of different things
         """ Sorting goes here"""
+        self.tripleList.sort(key= lambda x: x.sigma, reverse=True)
+        #we should now have a sorted list
     def test(self):
         testMatrix = np.zeros((self.m, self.n))
         for i in range(self.n):
             #to get back, we want sigma * ui * viT
             testMatrix = testMatrix + self.sigma[i]*np.outer(self.u[:,i], self.v[:,i])
+            
             #doing this entire summation should give us back our matrix
             #doing this up to a value k should give us the approximation
             #to get the best approx, we want these to be ordered by decreasing simga
-        print(testMatrix)
+        
+        #print(testMatrix)
+        return testMatrix
+    #used to test the SVD approximation for a matrix (still really good)
+    def testApprox(self):
+        testMatrix = np.zeros((self.m, self.n))
+        usedRange = min(self.n, 5)
+        for i in range(usedRange):
+            currSigma = self.tripleList[i].sigma
+            currV = self.tripleList[i].v
+            currU = self.tripleList[i].u
+            testMatrix = testMatrix + currSigma*np.outer(currU,currV)
         return testMatrix
             
 
-A = np.arange(10).reshape((2,5))
-print(A)
+A = np.arange(10000).reshape((100,100))
+#print(A)
 TestSVD = SVD(A)
-TestSVD.test()
 deltaA = A - TestSVD.test()
-print(deltaA)
+print(norm(deltaA)) #should be a small value that is about 0
+approxDeltaA = A - TestSVD.testApprox()
+print(norm(approxDeltaA))
                 
