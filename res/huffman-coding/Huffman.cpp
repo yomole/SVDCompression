ec124d7020fdf9f487f2b4d58b618e628d78bc7a
kyle <kyle.shechtman@ufl.edu>
2022-04-17 19:14:08 -0400

    init

---
+++ res/huffman-coding/Huffman.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Work on 4/17/22.
+//
+
+#include "Huffman.h"
---
+++ res/huffman-coding/Huffman.h
@@ -0,0 +1,14 @@
+//
+// Created by Work on 4/17/22.
+//
+
+#ifndef SVDCOMPRESSION_HUFFMAN_H
+#define SVDCOMPRESSION_HUFFMAN_H
+
+
+class Huffman {
+
+};
+
+
+#endif //SVDCOMPRESSION_HUFFMAN_H
