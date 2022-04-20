cc072fe6b27c0d1ed21bfed6caf4b06810a844c9
kyleshec <80716900+kyleshec@users.noreply.github.com>
2022-04-20 07:27:37 -0400

    Update Huffman.h

--- res/huffman-coding/Huffman.h
+++ res/huffman-coding/Huffman.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 struct Tree
@@ -19,7 +20,11 @@ struct Tree
 };
 
 class Huffman {
+    map<unsigned char, string> codes;
 public:
+    auto getCode(){
+        return codes;
+    }
     bool operator()(Tree* a, Tree* b)
     {
         return a->frequency > b->frequency;
@@ -33,9 +38,13 @@ string getHuffmanBitstring(unsigned char* buffer, map<unsigned char, string> cod
 unsigned char* getBufferFromString(string bitstring, vector<unsigned char>& outputBuffer, int& sz);
 string getStringFromBuffer(unsigned char* buffer, int sz);
 unsigned char* getDecodedBuffer(string bitstring, vector<unsigned char>& buffer, map<unsigned char, string> codes, int& sz, int paddedBits);
-void compressFile(const char* path,const char* output_path, map<unsigned char, string>& codes);
+void compressFile(const string &size, set<const char*> &files);
 void decompressFile(const char* inputPath, const char* outputPath);
 int CompareFiles(const char* pFname1,const char* pFname2);
-
+unsigned char* readFileIntoBuffer(const char* path, int& sz);
+void writeHeader(const char* path, map<unsigned char, string> codes, int paddedBits);
+unsigned char* readHeader(unsigned char* buffer, map<unsigned char, string>& codes, int& paddedBits, int& sz);
+void writeFileFromBuffer(const char* path, unsigned char* buffer, int sz, int flag);
+void traverseHuffmanTree(Tree* root, string prev, string toAppend, map<unsigned char, string> codemap);
 
 #endif //SVDCOMPRESSION_HUFFMAN_H
